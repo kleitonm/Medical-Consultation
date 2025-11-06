5d69b2ff0c9d45d32380aa760270abf3a5214554
Kleiton Mendes <kleitonmendes@MacBook-Air-de-Kleiton.local>
2025-11-06 19:41:57 -0300

    feat: create a ScheduleAppointmentView

--- Medical-Consultation/Medical-Consultation/Views/ScheduleAppointmentView.swift
+++ Medical-Consultation/Medical-Consultation/Views/ScheduleAppointmentView.swift
@@ -44,8 +44,64 @@ struct ScheduleAppointmentView: View {
         showAlert = false
     }
     
-    // MARK: - UI
-    var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    func scheduleAppointment() async {
+        do {
+            if let _ = try await  service.scheduleAppointment(specialistID: specialistID, patientID: patientID, date: selectedDate.convertToString()) {
+                isAppointmentScheduled = true
+            } else {
+                isAppointmentScheduled = false
+            }
+        } catch {
+            isAppointmentScheduled = false
+            print("Ocorreu um erro ao agendar consulta: \(error)")
+        }
+        showAlert = true
     }
+    
+    var body: some View {
+        VStack {
+            Text("Selecione a data e o horário da consulta")
+                .font(.title3)
+                .bold()
+                .foregroundStyle(.accent)
+                .multilineTextAlignment(.center)
+                .padding(.top)
+            
+            DatePicker("Escolha a data da consulta", selection: $selectedDate, in: Date()...)
+                .datePickerStyle(.graphical)
+            
+            Button(action: {
+                Task {
+                    if isRescheduleView {
+                        await rescheduleAppointment()
+                    } else {
+                        await scheduleAppointment()
+                    }
+                }
+            }, label: {
+                ButtonView(text: isRescheduleView ? "Reagendar consulta" : "Agendar consulta")
+            })
+        }
+        .padding()
+        .navigationTitle(isRescheduleView ? "Reagendar consulta" : "Agendar consulta")
+        .navigationBarTitleDisplayMode(.large)
+        .onAppear {
+            UIDatePicker.appearance().minuteInterval = 15
+        }
+        .alert(isAppointmentScheduled ? "Sucesso!" : "Ops, algo deu errado!", isPresented: $showAlert, presenting: isAppointmentScheduled) { _ in
+            Button(action: {}, label: {
+                Text("Ok")
+            })
+        } message: { isScheduled in
+            if isScheduled {
+                Text("A consulta foi \(isRescheduleView ? "reagendada" : "agendada") com sucesso!")
+            } else {
+                Text("Houve um erro ao \(isRescheduleView ? "reagendar" : "agendar") sua consulta. Por favor tente novamente ou entre em contato via telefone.")
+            }
+        }
+    }
+}
+
+#Preview {
+    ScheduleAppointmentView(specialistID: "123")
 }
