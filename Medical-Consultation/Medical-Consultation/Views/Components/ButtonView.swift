9338d42f8d18ccc01478cb72d5cdb379ca471984
Kleiton Mendes <kleitonmendes@MacBook-Air-de-Kleiton.local>
2025-11-06 19:53:13 -0300

    feat: continue WebService, and create a componets

---
+++ Medical-Consultation/Medical-Consultation/Views/Components/ButtonView.swift
@@ -0,0 +1,38 @@
+//
+//  ButtonView.swift
+//  Medical-Consultation
+//
+//  Created by Kleiton Mendes on 06/11/25.
+//
+
+import SwiftUI
+
+enum ButtonType {
+    case primary
+    case cancel
+}
+
+struct ButtonView: View {
+    var text: String
+    var buttonType: ButtonType
+    
+    init(text: String, buttonType: ButtonType = .primary) {
+        self.text = text
+        self.buttonType = buttonType
+    }
+    
+    var body: some View {
+        Text(text)
+            .bold()
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 14)
+            .background(buttonType == .primary ? Color.accentColor : Color(.cancel))
+            .cornerRadius(12)
+            .padding(.top, 8)
+    }
+}
+
+#Preview {
+    ButtonView(text: "Agendar consulta")
+}
